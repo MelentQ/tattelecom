316799198ff728b680f1114b4057731e023c66f1
gilyazov <gilyazov.aydar@gmail.com>
2022-06-23 10:45:27 +0300

    Slider Fix

--- src/js/finctions/initHeroSlider.js
+++ src/js/finctions/initHeroSlider.js
@@ -3,11 +3,17 @@ import { Navigation, Swiper, Pagination, Autoplay } from 'swiper';
 Swiper.use([Navigation, Pagination, Autoplay]);
 
 export default () => {
-    const autoplayValue = document.querySelector(".js-swiper-custom").getAttribute('data-swiper-autoplay')
-    let sliderHero = new Swiper(".js-swiper-custom", {
+    const sliderWrapper = document.querySelector('.js-swiper-custom');
+    const autoplayValue = sliderWrapper.getAttribute('data-swiper-autoplay');
+
+    const swiper = new Swiper(".js-swiper-custom", {
         speed: 400,
         autoplay: {
             delay: +autoplayValue,
+        },
+        navigation: {
+            prevEl: sliderWrapper.querySelector('.js-slider-btn-prev'),
+            nextEl: sliderWrapper.querySelector('.js-slider-btn-next')
         },
         pagination: {
 			el: '.rates-preview__sldier-pagination',
