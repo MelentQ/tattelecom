39f14fdd387f7519ec039f71d9b9b0ae47e58c31
gilyazov <gilyazov.aydar@gmail.com>
2023-05-29 14:37:15 +0300

    fix count button

--- src/js/finctions/initHeroSlider.js
+++ src/js/finctions/initHeroSlider.js
@@ -1,30 +1,36 @@
 import { Navigation, Swiper, Pagination, Autoplay } from 'swiper';
+import {Slider} from "../components/slider";
 
 Swiper.use([Navigation, Pagination, Autoplay]);
 
 export default () => {
-    const sliderWrapper = document.querySelector('.js-swiper-custom');
-    const autoplayValue = sliderWrapper.getAttribute('data-swiper-autoplay');
+    const sliderWrapperElements = document.querySelectorAll('.js-swiper-custom');
+    if (!sliderWrapperElements) return;
 
-    const numberOfSlides = sliderWrapper.querySelectorAll('.swiper-slide').length;
-    if(numberOfSlides === 1) {
-        sliderWrapper.querySelector('.js-slider-btn-prev').remove();
-        sliderWrapper.querySelector('.js-slider-btn-next').remove();
-    }
+    sliderWrapperElements.forEach(function(sliderWrapper) {
+        const autoplayValue = sliderWrapper.getAttribute('data-swiper-autoplay');
 
-    const swiper = new Swiper(".js-swiper-custom", {
-        speed: 400,
-        autoplay: {
-            delay: +autoplayValue,
-        },
-        navigation: {
-            prevEl: sliderWrapper.querySelector('.js-slider-btn-prev'),
-            nextEl: sliderWrapper.querySelector('.js-slider-btn-next')
-        },
-        pagination: {
-			el: '.rates-preview__sldier-pagination',
-            clickable: true,
-			type: "bullets",
-		},
+        const numberOfSlides = sliderWrapper.querySelectorAll('.swiper-slide').length;
+
+        if(numberOfSlides === 1) {
+            sliderWrapper.querySelector('.js-slider-btn-prev').remove();
+            sliderWrapper.querySelector('.js-slider-btn-next').remove();
+        }
+
+        const swiper = new Swiper(".js-swiper-custom", {
+            speed: 400,
+            autoplay: {
+                delay: +autoplayValue,
+            },
+            navigation: {
+                prevEl: sliderWrapper.querySelector('.js-slider-btn-prev'),
+                nextEl: sliderWrapper.querySelector('.js-slider-btn-next')
+            },
+            pagination: {
+                el: '.rates-preview__sldier-pagination',
+                clickable: true,
+                type: "bullets",
+            },
+        });
     });
 };
